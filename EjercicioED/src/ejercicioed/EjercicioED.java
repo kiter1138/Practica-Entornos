9e3c0aab7fa019f06a2e4c8346fd65e9530b60f7
jafrancom01 <jafrancom01@aula1-12>
2015-10-15 17:56:01 +0200

    ProyectoED

---
+++ EjercicioED/src/ejercicioed/EjercicioED.java
@@ -0,0 +1,18 @@
+package ejercicioed;
+
+/**
+ *
+ * @author jafrancom01
+ */
+public class EjercicioED {
+
+    public static void main(String[] args) {
+        byte edad= 22;
+        float altura= 1.73F;
+        
+        System.out.println(" Mi nombre es Jose Antonio Franco Martin ");
+        System.out.println(" Tengo " + edad + " años ");
+        System.out.println(" Mido " + altura+ " metros " );
+    }
+    
+}
