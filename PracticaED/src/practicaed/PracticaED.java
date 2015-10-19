69d66bfa636a867fa3a85902c33b2decc2d44cd1
jafrancom01 <jafrancom01@aula1-12>
2015-10-19 18:45:38 +0200

    ProyectoED

---
+++ PracticaED/src/practicaed/PracticaED.java
@@ -0,0 +1,18 @@
+package practicaed;
+
+/**
+ *
+ * @author jafrancom01
+ */
+public class PracticaED {
+
+    public static void main(String[] args) {
+       float numero1=0.5F;
+       long numero2;
+       float resultado;
+       numero2=178823419991L;
+       resultado=numero1*numero2;
+       System.out.println("El resultado del multiplicar " +numero1+"y" +numero2+ "es igual "+ resultado);
+    }
+    
+}
