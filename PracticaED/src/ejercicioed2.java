2bc5f910dfabd96f1b620ac0a0977a4e59f286c9
bacabanillasv01 <bacabanillasv01@aula1-11>
2015-10-20 19:48:35 +0200

    Ahí te mando el ejercicioed2

---
+++ PracticaED/src/ejercicioed2.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * @author bacabanillasv01
+ */
+import java.util.Scanner;
+
+public class ejercicioed2 {
+
+    void ejercicioed2() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int numero;
+
+        System.out.print("\t Introduce un número de cuatro cifras: ");
+
+        numero = teclado.nextInt();
+
+        int primer = numero / 1000;
+        int segundo = (numero % 1000) / 100;
+        int tercer = ((numero % 1000) % 100) / 10;
+        int cuarto = ((numero % 1000) % 100) % 10;
+
+        System.out.println("El primer dígito es: " + primer);
+        System.out.println("El segundo dígito es: " + segundo);
+        System.out.println("El tercer dígito es: " + tercer);
+        System.out.println("El cuarto dígito es: " + cuarto);
+    }
+
+}
