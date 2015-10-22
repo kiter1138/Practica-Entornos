5b0139c1bfd5eb96a289d528cb9fc8582ff3eba1
jafrancom01 <jafrancom01@aula1-12>
2015-10-22 17:37:08 +0200

    Cambios ejercicio4

--- PracticaED/src/practicaed/ejercicioed4.java
+++ PracticaED/src/practicaed/ejercicioed4.java
@@ -16,14 +16,14 @@ public class ejercicioed4 {
         
         dinero=teclado.nextInt();
         
-        int cincuenta=dinero/50;
-        int veinte=(dinero%50)/20;
-        int diez=((dinero%50)%20)/10;
-        int cinco=(((dinero%50)%20)%10)/5;
-        int dos=((((dinero%50)%20)%10)%5)/2;
-        int uno=((((dinero%50)%20)%10)%5)%2;
+        int b50=dinero/50;
+        int b20=(dinero%50)/20;
+        int b10=((dinero%50)%20)/10;
+        int b5=(((dinero%50)%20)%10)/5;
+        int m2=((((dinero%50)%20)%10)%5)/2;
+        int m1=((((dinero%50)%20)%10)%5)%2;
         
-        System.out.println(+ dinero + " euros se descomponen en " + cincuenta + " billete/s de 50€, " + veinte + " billete/s de 20€, " + diez + " billete/s de 10€, " + cinco + " billete/s de 5€, " + dos + " moneda/s de 2€ y " + uno + " moneda/s de 1€.");
+        System.out.println(+ dinero + " euros se descomponen en " + b50 + " billete/s de 50€, " + b20 + " billete/s de 20€, " + b10 + " billete/s de 10€, " + b5 + " billete/s de 5€, " + m2 + " moneda/s de 2€ y " + m1 + " moneda/s de 1€.");
         
     }
     
